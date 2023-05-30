b0a0653e7fdc1dff23f5347f4437ffacc8ab48b5
Aluel-Kuot <maryaluelkuot@gmail.com>
2023-05-30 16:48:05 +0300

    rest

---
+++ index.js
@@ -0,0 +1,31 @@
+
+let productContainer=document.getElementById('items')
+
+const getProduct=()=>{
+    return fetch('https://dummyjson.com/products')
+  .then(response=>response.json())
+  .then(response=>response)
+  .catch(error=>error.message)
+};
+
+
+const displayProducts=async()=>{
+  const items=  await getProduct();
+  console.log(items.items);
+  items.items.map(item=>{
+      let div=document.createElement('div')
+      div.className='product';
+      let img=document.createElement('img')
+      let products=document.createElement('h2')
+      let price=document.createElement('p')
+      img.src=items.images;
+      products.innerHTML=items.products
+      price.innerHTML=items.price;
+      div.appendChild(img);
+      div.appendChild(products);
+      div.appendChild(price);
+      productContainer.appendChild(div);
+
+  });
+};
+displayProducts();

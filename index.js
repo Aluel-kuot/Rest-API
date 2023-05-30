39c3417bdfd301fa29fed62bc03c8dcf4b96d810
Aluel-Kuot <maryaluelkuot@gmail.com>
2023-05-30 17:02:21 +0300

    rest

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const getProduct=()=>{
 const displayProducts=async()=>{
   const items=  await getProduct();
   console.log(items.items);
-  items.items.map(item=>{
+  items.items.map(item => {
       let div=document.createElement('div')
       div.className='product';
       let img=document.createElement('img')
       let products=document.createElement('h2')
       let price=document.createElement('p')
-      img.src=items.images;
-      products.innerHTML=items.products
-      price.innerHTML=items.price;
+      img.src=item.images;
+      products.innerHTML=item.products
+      price.innerHTML=item.price;
       div.appendChild(img);
       div.appendChild(products);
       div.appendChild(price);
